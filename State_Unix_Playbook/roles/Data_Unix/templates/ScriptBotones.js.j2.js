fa9c3e9b186cb5ba2e8a37bf0758fc5793f38637
unixbd <61426474+unixbd@users.noreply.github.com>
2020-03-17 11:03:20 -0500

    Update ScriptBotones.js.j2.js

--- State_Unix_Playbook/roles/Data_Unix/templates/ScriptBotones.js.j2.js
+++ State_Unix_Playbook/roles/Data_Unix/templates/ScriptBotones.js.j2.js
@@ -1,12 +1,12 @@
 
 <script>
     const btnOpen{{ ansible_hostname }} = document.querySelector('#Open-{{ ansible_hostname }}'),
-    Table{{ ansible_hostname }} = document.querySelector('#Table-{{ ansible_hostname }}');
+    Table{{ ansible_hostname }} = document.querySelector('#Table-{{ ansible_hostname }}'), Cont{{ ansible_hostname }} = document.querySelector('#al200235049scontenedor-tabla');
     btnOpen{{ ansible_hostname }}.addEventListener('click',() => {
         $('.tabla').each(function() {
             if ( $(this).hasClass("Table-{{ ansible_hostname }}") ) {
                 Table{{ ansible_hostname }}.classList.toggle('active');
-                $(".{{ ansible_hostname }}contenedor-tabla").toggle('active')
+                Cont{{ ansible_hostname }}.classList.toggle('active2')
                 $('.htable').removeClass('active')
             }else{
                 $(this).removeClass('active')
