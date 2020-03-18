a6f0873532a9132233862d4a2a4fe4cde6db522a
unixbd <61426474+unixbd@users.noreply.github.com>
2020-03-18 16:27:00 -0500

    Update ScriptBotones.js.j2.js

--- State_Windows_Playbook/roles/Data_Win/templates/ScriptBotones.js.j2.js
+++ State_Windows_Playbook/roles/Data_Win/templates/ScriptBotones.js.j2.js
@@ -11,6 +11,12 @@
                 $('.htable').removeClass('active');
             }else{
                 $(this).removeClass('active');
+            }
+        });
+        $('.divtabla').each(function() {
+            if ( $(this).hasClass("{{ ansible_env.COMPUTERNAME }}-contenedor-tabla") ) {
+                Cont{{ ansible_env.COMPUTERNAME }}.classList.toggle('active2');
+            }else{
                 $(this).removeClass('active2');
             }
         });
